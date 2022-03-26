face540aa1aeadd5255b49566effb6d91482386d
chandan786786 <chandan101288@gmail.com>
2022-03-26 13:32:11 +0530

    first commit

---
+++ app/src/main/java/org/nic/lmd/officerapp/RandomMarInspectionActivity.java
@@ -0,0 +1,14 @@
+package org.nic.lmd.officerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RandomMarInspectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_random_mar_inspection);
+    }
+}
